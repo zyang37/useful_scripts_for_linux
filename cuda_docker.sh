3fc434e691c6b80931ebc28f70c534c9479a34d9
Zhenning Yang <zhenningyang0139@gmail.com>
2020-04-04 00:03:17 -0400

    add link to cuda

--- cuda_docker.sh
+++ cuda_docker.sh
@@ -15,6 +15,7 @@ elif [ "$1" = "$remove" ]; then
   sudo docker rm cuda
 elif [ "$1" = "$fix_gpu_error" ]; then
   # Add the package repositories
+  # https://github.com/NVIDIA/nvidia-docker
   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
   curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
