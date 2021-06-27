f775c78cde755fd5ab96b233081b97192f5c0f23
Zhenning Yang <zhenningyang0139@gmail.com>
2021-06-27 18:39:03 -0500

    ...

--- Docker4pt/docker_run.sh
+++ Docker4pt/docker_run.sh
@@ -11,7 +11,7 @@ image="pt-z"
 
 if [ "$1" = "$run" ]; then
   docker run --gpus all -it \
-	-p 8889:8889 \
+	-p 8889:8888 \
 	-p 6007:6006 \
 	-v /home/zyang37/:/home/zyang37/ \
 	-v /da2_data/shared/twitter/:/da2_data/shared/twitter/ \
