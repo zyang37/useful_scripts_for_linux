3ae686ce57afafe55e7d8b0c29b039de455fbd25
Zhenning Yang <zhenningyang0139@gmail.com>
2022-07-20 22:55:00 -0400

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -7,6 +7,7 @@ conda update conda
 
 # make conda install faster
 conda config --add channels conda-forge
-conda config --set channel_priority strict
+#conda config --set channel_priority strict
+#conda config --set channel_priority true 
 
 echo "done"
