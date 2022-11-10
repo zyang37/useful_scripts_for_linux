66b308797abdb7edc63187e7425ef67b713c4257
Zhenning Yang <zhenningyang0139@gmail.com>
2022-11-10 11:34:09 -0500

    ...

---
+++ install_pt_dev_pkgs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
+
+pip3 install torch torchvision torchaudio
+
+pip install pandas
+
+pip3 install -U scikit-learn
+
+pip install jupyterlab
+
+conda install -y matplotlib
+
+pip install opencv-python
+
+pip install tqdm
